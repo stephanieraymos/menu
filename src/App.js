d26f4baf01c62b79907d2cbd68364abd50bbb229
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-19 16:04:26 -0800

    App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Catergories";
+import items from "./data";
+
+function App() {
+  document.title = "Menu";
+  return <h2>Menu</h2>;
+}
+
+export default App;
