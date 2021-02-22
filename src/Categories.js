a372b5f7dd00a18f3d695d91df406e72b25243bb
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-22 10:04:21 -0800

    Category buttons

--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Categoies = () => {
-  return <h2>categoies component</h2>;
+const Categoies = ({filterItems}) => {
+  return <div className="btn-container">
+    <button className="filter-btn" onClick={() => filterItems('breakfast')}>Breakfast</button>
+    <button className="filter-btn" onClick={() => filterItems('lunch')}>Lunch</button>
+    <button className="filter-btn" onClick={() => filterItems('shakes')}>Shakes</button>
+  </div>;
 };
 
 export default Categoies;
